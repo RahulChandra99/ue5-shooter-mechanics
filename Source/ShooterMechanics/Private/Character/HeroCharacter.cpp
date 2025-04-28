eb73c2519bfb561fee9166eee00f10d7e85aa623
Rahul Chandra <60950973+RahulChandra99@users.noreply.github.com>
2025-04-28 14:21:39 +0100

    multi test

--- Source/ShooterMechanics/Private/Character/HeroCharacter.cpp
+++ Source/ShooterMechanics/Private/Character/HeroCharacter.cpp
@@ -1,31 +1,37 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Character/HeroCharacter.h"
+#include "ShooterMechanics/Public/Character//HeroCharacter.h"
 
-// Sets default values
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
 AHeroCharacter::AHeroCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 300.f;
+	CameraBoom->bUsePawnControlRotation = true;
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom);
+	FollowCamera->bUsePawnControlRotation = false;
 
 }
 
-// Called when the game starts or when spawned
 void AHeroCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
 void AHeroCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
-// Called to bind functionality to input
 void AHeroCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
